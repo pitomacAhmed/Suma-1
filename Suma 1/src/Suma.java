cd20012370b1a72ac44db694495ee3e691a24fcf
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 17:50:30 +0100

    Add files via upload

---
+++ Suma 1/src/Suma.java
@@ -0,0 +1,27 @@
+
+public class Suma {
+
+	public static void main(String[] args) {
+		int a,b,c,d,e;
+		int sumap=0, suman=0;
+		Scanner tastatura = new Scanner (System.in);
+		System.out.println("Vnesete go prviot broj:");
+		a=tastatura. nextInt();
+		System.out.println("Vnesete vtoriot broj;");
+		b=tastatura. nextInt();
+		System.out.println("Vnesete go tretiot broj:");
+		c=tastatura. nextInt();
+		System.out.println("Vnesete go cetvrtiot broj:");
+		d=tastatura. nextInt();
+		System.out.println("Vnesete go petiot broj:");
+		e=tastatura. nextInt();
+		if(a%2==0) {
+			sumap=suman+e;
+		} else {
+			sumap=suman+e;
+		}
+		System.out.println("Sumata na parnite broevi e:");
+		System.out.println("Sumata na neparnite broevi e:");}
+
+}
+
